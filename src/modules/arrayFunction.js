bf2ea70904d4e62d9ca917522aa13900e66d724f
Dudarev <dima_dudariev@mail.ru>
2020-08-04 13:00:09 +0300

    1 Section is ready

--- src/modules/arrayFunction.js
+++ src/modules/arrayFunction.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-debugger */
 console.log('Array functions\n\n')
 
 const arrayFunction = () => {
@@ -29,25 +30,49 @@ const arrayFunction = () => {
   console.log('END 1.2\n\n')
 
   console.log('1.3. Сортировка к максимальному значению [просто]')
-  const biggest = numbs => {
-    console.log('numbs: ', numbs)
-    if (numbs.length === 0) {
-      return []
-    } if (numbs.length === 1) {
-      return numbs
-    } else {
-      const rezult = []
-      const allCassesOfRest = biggest(numbs.slice(1))
-      for (var c in allCassesOfRest) {
-        for (var i = 0; i < numbs.length; i++) {
-          rezult.push(numbs[i] + allCassesOfRest[c])
-        }
-      }
-      return rezult
-    }
-  }
+  const biggest = numbs => numbs.sort().reverse().join('')
   console.log(biggest(['3', '30', '34', '5', '9']))
   console.log('END 1.3\n\n')
+
+  console.log('1.4. Комбинации сдвига [просто]')
+  const pattern = amount => {
+    return new Array(amount).fill(null)
+      .reduce((accum, _item, i) => {
+        accum[i] = i + 1
+        return accum
+      }, [])
+      .reduce((accum, item, i, array) => {
+        accum.push(array.slice(i, array.length).join('') + array.slice(0, i).join(''))
+        return accum
+      }, [])
+  }
+  console.log('pattern: ', pattern(5))
+  console.log('END 1.4\n\n')
+
+  console.log('1.5. Арифметическая последовательность - сумма из n элементов[просто]')
+  const arithmeticSequenceSum = (a, r, n) => {
+    return new Array(n).fill(null)
+      // eslint-disable-next-line no-return-assign
+      .reduce((accum, _item, i) => accum += new Array(i).fill(null).reduce((acc) => acc += r, a), 0)
+  }
+  console.log('arithmeticSequenceSum: ', arithmeticSequenceSum(2, 3, 5))
+  console.log('END 1.5\n\n')
+
+  console.log('1.6. Диагональная сумма матрицы [просто]')
+  const diagonalSum = arrays => {
+    return arrays.reduce((accum, item, i) => {
+      accum += item[i]
+      return accum
+    }, 0)
+  }
+  console.log('diagonalSum: ', diagonalSum([
+    [5, 9, 1, 0],
+    [8, 7, 2, 3],
+    [1, 4, 1, 9],
+    [2, 3, 8, 2]
+  ]
+  ))
+  console.log('END 1.6\n\n')
 }
 
 export default arrayFunction
